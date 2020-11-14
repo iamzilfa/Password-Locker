33ea9479981db6d5739041c96fa66f2a3a9db9c4
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-14 19:55:43 +0200

    creating new contact in run

--- run.py
+++ run.py
@@ -5,3 +5,9 @@ import random
 import string
 import sys
 
+def create_user(username,password):
+    """
+    Function to create a new user
+    """
+    new_user = User(username,password)
+    return new_user
